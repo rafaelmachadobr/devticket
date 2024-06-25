83924157c71940de74d4b00edd71d6208d1c5bf6
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2024-06-24 21:53:53 -0300

    feat: Add domain models for events, spots, and tickets

---
+++ golang/go.mod
@@ -0,0 +1,3 @@
+module github.com/rafaelmachadobr/devticket/golang
+
+go 1.22.4
---
+++ golang/internal/events/domain/event.go
@@ -0,0 +1,39 @@
+package domain
+
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrEventNameRequired = errors.New("event name is required")
+	ErrEventDateFuture   = errors.New("event date must be in the future")
+	ErrEventCapacityZero = errors.New("event capacity must be greater than zero")
+	ErrEventPriceZero    = errors.New("event price must be greater than or equal to zero")
+)
+
+type Rating string
+
+const (
+	RatingLivre Rating = "L"
+	Rating10    Rating = "L10"
+	Rating12    Rating = "L12"
+	Rating14    Rating = "L14"
+	Rating16    Rating = "L16"
+	Rating18    Rating = "L18"
+)
+
+type Event struct {
+	ID           string
+	Name         string
+	Location     string
+	Organization string
+	Rating       Rating
+	Date         time.Time
+	ImageURL     string
+	Capacity     int
+	Price        float64
+	PartnerID    int
+	Spot         []Spot
+	Tickets      []Ticket
+}
